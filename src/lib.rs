b3e26cce7a6b48923eebb225e5c820a19b210c5d
friaca <v.fria@outlook.com>
2022-04-10 23:09:11 -0300

    [change] formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
 use regex::Regex;
-use serde_json::Value;
 use reqwest::header::USER_AGENT;
+use serde_json::Value;
 
 mod response;
+use response::BasicListingVec;
+
 mod template;
 
 use std::error::Error;
@@ -58,15 +60,21 @@ fn format_reddit_media_url(media_id: &str, extension: &str) -> String {
 
 fn get_media_filename_from_url(url: &str) -> String {
   let filename_regex = Regex::new(r"/([\w\d]{13}\.\w+)$").unwrap();
-  filename_regex.captures(url).unwrap().get(1).unwrap().as_str().to_owned()
+  filename_regex
+    .captures(url)
+    .unwrap()
+    .get(1)
+    .unwrap()
+    .as_str()
+    .to_owned()
 }
 
 fn try_save_images(post_content: &Value, path: &std::path::PathBuf) -> Result<(), Box<dyn Error>> {
   if post_content["is_reddit_media_domain"].as_bool().unwrap() {
     save_image(
-      &post_content["url"].as_str().unwrap(), 
-      &get_media_filename_from_url(&post_content["url"].as_str().unwrap()), 
-      path
+      &post_content["url"].as_str().unwrap(),
+      &get_media_filename_from_url(&post_content["url"].as_str().unwrap()),
+      path,
     )?
   } else if post_content["is_gallery"].as_bool().unwrap() {
     let image_urls = get_gallery_urls(&post_content["media_metadata"]);
@@ -87,10 +95,15 @@ fn get_gallery_urls(media_metadata: &Value) -> Vec<String> {
     for (_, value) in media.iter() {
       // value["m"] == "image/png" | "image/jpg" | ...
       let media_type = &value["m"].as_str().unwrap()[..6];
-      if media_type != "image/" { continue; }
+      if media_type != "image/" {
+        continue;
+      }
 
       let extension = &value["m"].as_str().unwrap()[6..];
-      urls.push(format_reddit_media_url(value["id"].as_str().unwrap(), &extension));
+      urls.push(format_reddit_media_url(
+        value["id"].as_str().unwrap(),
+        &extension,
+      ));
     }
   }
 
@@ -121,9 +134,7 @@ fn save_file(content: String, path: std::path::PathBuf) {
     .expect("Couldn't write file, do you have rights do modify files in the provived output path?");
 }
 
-fn get_reddit_post(
-  url: &str,
-) -> Result<response::BasicListingVec<serde_json::Value>, Box<dyn Error>> {
+fn get_reddit_post(url: &str) -> Result<BasicListingVec<serde_json::Value>, Box<dyn Error>> {
   let client = reqwest::blocking::Client::new();
   let response = client
     .get(url)
@@ -150,6 +161,6 @@ pub fn run(args: CliArgs) -> Result<(), Box<dyn Error>> {
 
   save_file(file_content, args.output);
   println!("Post saved succesfully!");
-  
+
   Ok(())
 }
